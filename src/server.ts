8851663f77bcfea03229137bc90ef5cc6d5b6846
Roni Chandra Sarkar <ronichandrasarkar@gmail.com>
2023-05-30 21:22:48 +0530

    [done] eslint prettier

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import { connect } from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import { connect } from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function run() {
   try {
-    await connect(config.database_url as string);
-    console.log("Database connection successful");
+    await connect(config.database_url as string)
+    console.log('Database connection successful')
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("database is not connected");
+    console.log('database is not connected')
   }
 }
-run().catch((err) => console.log(err));
+run().catch(err => console.log(err))
